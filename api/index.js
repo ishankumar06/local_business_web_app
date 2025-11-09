157aa0a29ee831f28b55f4dd51c81f8bdd36b411
ishankumar06 <ishansingh06022004@gmail.com>
2025-11-09 22:04:46 +0530

    changes

--- api/index.js
+++
@@ -1,3 +0,0 @@
-import { handler } from '../backend/server.js';  
-
-export { handler };
--- backend/server.js
+++ backend/server.js
@@ -5,28 +5,36 @@ import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
 import userRouter from './routes/userRoute.js'
 import productRouter from './routes/productRoute.js'
-import cartRouter from './routes/cartRoute.js'
+import cartRouter from './routes/cartRoute.js';
 import orderRouter from './routes/orderRoute.js'
-import serverless from 'serverless-http'  
+
+//app config
 
 const app = express()
 
+const port = process.env.PORT || 4000
 connectDB()
 connectCloudinary()
+
+
+//middleware
+
 
 app.use(express.json())
 app.use(cors())
 
-app.use('/api/user', userRouter)
-app.use('/api/product', productRouter)
-app.use('/api/cart', cartRouter)
-app.use('/api/order', orderRouter)
 
-app.get('/', (req, res) => {
-  res.send("API working")
+//api endpoints
+app.use('/api/user',userRouter)
+app.use('/api/product',productRouter)
+app.use('/api/cart', cartRouter);
+app.use('/api/order', orderRouter);
+
+
+app.get('/',(req,res)=>{
+   
+    res.send("API working")
 })
 
-// Remove app.listen()
+app.listen(port,()=>console.log('server started on port : '+port))
 
-// Export the app wrapped in serverless-http
-export const handler = serverless(app)
